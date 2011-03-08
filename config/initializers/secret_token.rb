04cac9cf06cf5139e37f3d816adca946ccb4b018
Guo Lei <guolei@idapted.com>
2011-03-08 10:32:42 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Getbuz::Application.config.secret_token = '592d5fb1171ae50fd88c6b1ad242aab05b05a4384433a59a0d3dcf76db225b4d421af06b6627a2444ef7c99a9c5db510dd748d3544b9ec1a2f6bd5981cf6b393'
